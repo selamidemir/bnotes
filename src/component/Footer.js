fbb46d2847f3b00d14b08582363c65abfc2cb858
Selami Demir <selamidemir@gmail.com>
2022-07-28 18:14:38 +0300

    Footer bolumune tasarimci bilgisi eklendi

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -5,7 +5,7 @@ import { Container } from 'react-bootstrap';
 function Footer() {
   return (
     <Container fluid className='bg-light border-top fixed-bottom p-3'> { /* position-absolute bottom-0 end-0 py-4 */ }
-      <div className='text-center '>Selami Demir</div>
+      <div className='text-center '>Desinged By Selami Demir</div>
     </Container>
   )
 }
